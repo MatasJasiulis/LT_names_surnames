5ab88dc1312cacd335af9154d4ed953cf766ff6c
Matas Jasiulis <matas.jasiulis@gmail.com>
2013-05-30 22:00:16 +0300

    LT naming init
    Function changes Lithuanian names to call them example, Vardenis changes
    to Vardeni, Pavardenis changes to Pavardeni. Jonas Jonaitis to Jonai
    Jonaiti.
    Used on mails, or other places for greeting

---
+++ naming.php
@@ -0,0 +1,38 @@
+﻿<?php
+
+
+function name_change($sName){
+    
+    mb_internal_encoding('UTF-8');
+    
+    $aEnds = array ("as", "us", "ys", "is","ė");
+    $aEndRep= array ("ai", "au", "y", "i","e" );
+    
+    $sName=trim(mb_strtolower($sName));    
+    $sName=  str_replace("-"," ",$sName);
+    $sName=  str_replace(","," ",$sName);
+    $sName=  str_replace("  "," ",$sName);
+    $aNames = explode (' ', $sName);
+   
+    $sName="";
+        
+    foreach($aNames as $sFName){
+        
+         if(mb_substr($sFName, -1, 1 )=="ė"){
+             $sEnd="ė";
+         }
+         else{
+             $sEnd=mb_substr($sFName, -2, 2 );   
+         }     
+              
+         if(in_array($sEnd, $aEnds)){
+             $sRep=str_replace( $aEnds, $aEndRep, $sEnd);
+             $sName.=mb_ucfirst(mb_substr($sFName, 0, mb_strlen($sFName)-mb_strlen($sEnd)).$sRep)." ";
+         }
+         else{
+             $sName.=mb_ucfirst($sFName)." ";
+         }
+    }
+    
+    return $sName;
+}
